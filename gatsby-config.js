792b1f3f8c3b6d554496b431806acdadc783bddf
Mustafa Sarac <mrsarac@gmail.com>
2023-02-03 15:39:16 +0100

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,23 @@
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "G-Q5GQHSWLE6",
+        head: true,
+        anonymize: true,
+      },
+    },
+    {
       resolve: `gatsby-theme-garden`,
       options: {
         contentPath: `${__dirname}/pages`,
         rootNote: `/000 Digital Garden`,
       },
-    },
-    
+    }
   ],
   siteMetadata: {
     title: `Mustafa Saraç - Digital Garden`,
   },
+  
 }
